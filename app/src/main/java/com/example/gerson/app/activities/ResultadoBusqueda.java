5c6d3c01ca8e1454d608acd1dd634f37b85cddc7
Louiso <Gransullca.25@gmail.com>
2019-01-10 14:43:09 -0500

    se agrego modal

--- app/src/main/java/com/example/gerson/app/activities/ResultadoBusqueda.java
+++ app/src/main/java/com/example/gerson/app/activities/ResultadoBusqueda.java
@@ -1,10 +1,12 @@
 package com.example.gerson.app.activities;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.ScrollView;
 
 import com.example.gerson.app.R;
@@ -14,16 +16,23 @@ import com.example.gerson.app.models.Artefacto;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Home extends AppCompatActivity {
+public class ResultadoBusqueda extends AppCompatActivity {
     public List<Artefacto> artefactos;
     private ScrollView scroll;
+    private Dialog dialog;
+    private LinearLayout carrito;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_resultado_busqueda);
 
-        scroll = (ScrollView)findViewById(R.id.scrollHome);
+        scroll = (ScrollView)findViewById(R.id.scroll);
         scroll.fullScroll(ScrollView.FOCUS_UP);
+
+        dialog = new Dialog(this);
+        dialog.setContentView(R.layout.artefacto_detalles);
+        carrito = (LinearLayout)findViewById(R.id.carrito_id);
+
         artefactos = new ArrayList<>();
         artefactos.add(new Artefacto("Xiami Pocophone F1", 1479.00,
                 "Snapdragon 660 octa-core Kryo 2.2 + 1.8 Ghz", "Adreno 512",
@@ -56,7 +65,7 @@ public class Home extends AppCompatActivity {
                 "3.010 mAh Quick Charge 3.0", R.drawable.xiaomi
         ));
         RecyclerView artefactosList = (RecyclerView)findViewById(R.id.ArtefactosList);
-        ArtefactoCardItemAdapter adapter = new ArtefactoCardItemAdapter(this,artefactos, null);
+        ArtefactoCardItemAdapter adapter = new ArtefactoCardItemAdapter(this,artefactos, dialog);
         artefactosList.setLayoutManager(new GridLayoutManager(this,2));
         artefactosList.setAdapter(adapter);
 
--- app/src/main/java/com/example/gerson/app/adapters/ArtefactoCardItemAdapter.java
+++ app/src/main/java/com/example/gerson/app/adapters/ArtefactoCardItemAdapter.java
@@ -1,6 +1,9 @@
 package com.example.gerson.app.adapters;
 
+import android.animation.ArgbEvaluator;
+import android.app.Dialog;
 import android.content.Context;
+import android.media.Image;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -8,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.gerson.app.R;
 import com.example.gerson.app.models.Artefacto;
@@ -20,10 +24,11 @@ public class ArtefactoCardItemAdapter extends RecyclerView.Adapter<ArtefactoCard
 
     private Context mContext;
     private List<Artefacto> mData;
-
-    public ArtefactoCardItemAdapter(Context mContext, List<Artefacto> mData){
+    private Dialog dialog;
+    public ArtefactoCardItemAdapter(Context mContext, List<Artefacto> mData, Dialog dialog){
         this.mContext = mContext;
         this.mData = mData;
+        this.dialog = dialog;
     }
 
     @NonNull
@@ -32,7 +37,7 @@ public class ArtefactoCardItemAdapter extends RecyclerView.Adapter<ArtefactoCard
         View view;
         LayoutInflater mInflater = LayoutInflater.from(mContext);
         view = mInflater.inflate(R.layout.mycard_item, viewGroup, false);
-        return new ArtefactoCardItem(view);
+        return new ArtefactoCardItem(view, mContext, dialog);
     }
 
     @Override
@@ -52,13 +57,27 @@ public class ArtefactoCardItemAdapter extends RecyclerView.Adapter<ArtefactoCard
         TextView artefacto_titulo;
         TextView artefacto_precio;
         ImageView artefacto_img;
+        ImageView artefacto_lupa_img;
 
-        public ArtefactoCardItem(View view){
+        private Context mContext;
+        private Dialog dialog;
+        public ArtefactoCardItem(View view, Context context, final Dialog dialog){
             super(view);
+            this.dialog = dialog;
+            this.mContext = context;
 
             artefacto_img = (ImageView)itemView.findViewById(R.id.artefacto_img_id);
             artefacto_titulo = (TextView)itemView.findViewById(R.id.artefacto_text_titulo_id);
             artefacto_precio = (TextView)itemView.findViewById(R.id.artefacto_text_precio_id);
+            artefacto_lupa_img = (ImageView)itemView.findViewById(R.id.artefacto_lupa_icon_id);
+
+            artefacto_lupa_img.setOnClickListener(new View.OnClickListener(){
+                public void onClick(View view){
+
+                    dialog.show();
+
+                }
+            });
 
         }
     }
