9c633586b7d50009c72788d23682b0f7b6273a3d
Louiso <Gransullca.25@gmail.com>
2019-01-11 11:35:23 -0500

    acabado

--- app/src/main/java/com/example/gerson/app/activities/Main2Activity.java
+++ app/src/main/java/com/example/gerson/app/activities/Main2Activity.java
@@ -13,6 +13,7 @@ import com.example.gerson.app.models.Celular;
 import com.squareup.picasso.Picasso;
 
 public class Main2Activity extends AppCompatActivity {
+
     private  Celular mCelular;
     private  ImageView imgFoto;
     private  TextView  etCosto;
@@ -22,32 +23,35 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
         imgFoto=(ImageView)findViewById(R.id.imageviewcel);
         etCosto=(TextView) findViewById(R.id.textViewCost);
         etDescripcion=(TextView) findViewById(R.id.textViewDescripcion);
         etMarca=(TextView) findViewById(R.id.textViewMarca);
 
-
-
         configActionBar();
-        Intent intent=getIntent();
+        Intent intent = getIntent();
         configArtista();
 
     }
+
     private void configActionBar() {
+
         ActionBar actionBar = getSupportActionBar();
+
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
     }
-    private void configArtista() {
-        mCelular = MainActivity.sPhone;
-    mCelular.setDescripcion("adadad a");
-     etMarca.setText(mCelular.getName());
-     etDescripcion.setText(mCelular.getDescripcion());
-     etCosto.setText(mCelular.getPrecio());
-     imgFoto.setImageResource(mCelular.getPoster());
 
+    private void configArtista() {
+
+        mCelular = MainActivity.sPhone;
+        mCelular.setDescripcion("adadad a");
+        etMarca.setText(mCelular.getName());
+        etDescripcion.setText(mCelular.getDescripcion());
+        etCosto.setText(mCelular.getPrecio());
+        imgFoto.setImageResource(mCelular.getPoster());
     }
 
 
--- app/src/main/java/com/example/gerson/app/activities/ResultadoBusqueda.java
+++ app/src/main/java/com/example/gerson/app/activities/ResultadoBusqueda.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
@@ -18,16 +20,16 @@ import java.util.List;
 
 public class ResultadoBusqueda extends AppCompatActivity {
     public List<Artefacto> artefactos;
-    private ScrollView scroll;
     private Dialog dialog;
     private LinearLayout carrito;
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado_busqueda);
 
-        scroll = (ScrollView)findViewById(R.id.scroll);
-        scroll.fullScroll(ScrollView.FOCUS_UP);
+        toolbar = (Toolbar)findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
 
         dialog = new Dialog(this);
         dialog.setContentView(R.layout.artefacto_detalles);
@@ -44,15 +46,15 @@ public class ResultadoBusqueda extends AppCompatActivity {
                 20.0, "IPS, 5.99’’", "4 / 6 GB", "32 / 64 / 128",
                 "3.010 mAh Quick Charge 3.0", R.drawable.xiaomi
         ));
-        artefactos.add(new Artefacto("Xiami Pocophone F1", 1479.00,
-                "Snapdragon 660 octa-core Kryo 2.2 + 1.8 Ghz", "Adreno 512",
-                20.0, "IPS, 5.99’’", "4 / 6 GB", "32 / 64 / 128",
-                "3.010 mAh Quick Charge 3.0", R.drawable.sansung
+        artefactos.add(new Artefacto("LG V35 ThinQ", 1599.00,
+                "Snapdragon 845 octa-core", "Adreno 512",
+                16.0, "IPS, 6’’", "6 GB", "64",
+                "3.300 mAh", R.drawable.lg_v35_thinq
         ));
-        artefactos.add(new Artefacto("Xiaomi Mia A2", 789.00,
-                "Snapdragon 660 octa-core Kryo 2.2 + 1.8 Ghz", "Adreno 512",
-                20.0, "IPS, 5.99’’", "4 / 6 GB", "32 / 64 / 128",
-                "3.010 mAh Quick Charge 3.0", R.drawable.xiaomi
+        artefactos.add(new Artefacto("Xiaomi Redmi 6", 529.00,
+                "Helio P22 de ocho nucleos", "Adreno 512",
+                12.0, "IPS, 5.45’’", "3 GB", "32",
+                "3.000 mAh Quick Charge 3.0", R.drawable.xiaomi_redmi_6
         ));
         artefactos.add(new Artefacto("Xiami Pocophone F1", 1479.00,
                 "Snapdragon 660 octa-core Kryo 2.2 + 1.8 Ghz", "Adreno 512",
@@ -65,9 +67,14 @@ public class ResultadoBusqueda extends AppCompatActivity {
                 "3.010 mAh Quick Charge 3.0", R.drawable.xiaomi
         ));
         RecyclerView artefactosList = (RecyclerView)findViewById(R.id.ArtefactosList);
+        artefactosList.setFocusable(false);
         ArtefactoCardItemAdapter adapter = new ArtefactoCardItemAdapter(this,artefactos, dialog);
         artefactosList.setLayoutManager(new GridLayoutManager(this,2));
         artefactosList.setAdapter(adapter);
 
+    }
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.my_menu,menu);
+        return true;
     }
 }
