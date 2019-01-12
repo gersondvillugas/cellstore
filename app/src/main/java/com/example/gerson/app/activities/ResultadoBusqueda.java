893e18d1966cf48aec598cc70b57f73982b3aa5c
Louiso <Gransullca.25@gmail.com>
2019-01-12 18:12:47 -0500

    se agrego feedback de dardos

--- app/src/main/java/com/example/gerson/app/activities/ResultadoBusqueda.java
+++ app/src/main/java/com/example/gerson/app/activities/ResultadoBusqueda.java
@@ -1,22 +1,28 @@
 package com.example.gerson.app.activities;
 
 import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.ColorInt;
+import android.support.annotation.ColorLong;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.example.gerson.app.R;
 import com.example.gerson.app.adapters.ArtefactoCardItemAdapter;
 import com.example.gerson.app.models.Artefacto;
+import com.example.gerson.app.models.Carrito;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -82,13 +88,25 @@ public class ResultadoBusqueda extends AppCompatActivity {
                 20.0, "IPS, 5.99’’", "4 / 6 GB", "32 / 64 / 128",
                 "3.010 mAh Quick Charge 3.0", R.drawable.xiaomi
         ));
+
         RecyclerView artefactosList = (RecyclerView)findViewById(R.id.ArtefactosList);
         artefactosList.setFocusable(false);
         ArtefactoCardItemAdapter adapter = new ArtefactoCardItemAdapter(this,artefactos, dialog);
         artefactosList.setLayoutManager(new GridLayoutManager(this,2));
         artefactosList.setAdapter(adapter);
 
+        resultadosView.setText("Resultados: " + artefactos.size());
+        Carrito.getInstance().carritoFeedBack(this);
     }
+
+    public void verificarCambios(){
+        for(Artefacto artefacto : artefactos){
+
+        }
+    }
+
+
+
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.my_menu,menu);
         return true;
@@ -98,4 +116,69 @@ public class ResultadoBusqueda extends AppCompatActivity {
         Intent intent = new Intent(this, ListCarrito.class);
         startActivity(intent);
     }
+
+    public List<Artefacto> getArtefactos() {
+        return artefactos;
+    }
+
+    public void setArtefactos(List<Artefacto> artefactos) {
+        this.artefactos = artefactos;
+    }
+
+    public Dialog getDialog() {
+        return dialog;
+    }
+
+    public void setDialog(Dialog dialog) {
+        this.dialog = dialog;
+    }
+
+    public LinearLayout getCarrito() {
+        return carrito;
+    }
+
+    public void setCarrito(LinearLayout carrito) {
+        this.carrito = carrito;
+    }
+
+    public TextView getResultadosView() {
+        return resultadosView;
+    }
+
+    public void setResultadosView(TextView resultadosView) {
+        this.resultadosView = resultadosView;
+    }
+
+    public LinearLayout getFiltroView() {
+        return filtroView;
+    }
+
+    public void setFiltroView(LinearLayout filtroView) {
+        this.filtroView = filtroView;
+    }
+
+    public LinearLayout getCarritoView() {
+        return carritoView;
+    }
+
+    public void setCarritoView(LinearLayout carritoView) {
+        this.carritoView = carritoView;
+    }
+
+    public ImageView getCarritoImageView() {
+        return carritoImageView;
+    }
+
+    public void setCarritoImageView(ImageView carritoImageView) {
+        this.carritoImageView = carritoImageView;
+    }
+
+    public TextView getCarritoTextView() {
+        return carritoTextView;
+    }
+
+    public void setCarritoTextView(TextView carritoTextView) {
+        this.carritoTextView = carritoTextView;
+    }
+
 }
